9e5cad5251796512cdcfdfde0b5150b7a29e8dd2
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-10-23 13:19:11 +0200

    FDP-1622: Fix pipeline
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- application/src/test/kotlin/org/gxf/standalonenotifyinggateway/coaphttpproxy/MessageHandlerTest.kt
+++ application/src/test/kotlin/org/gxf/standalonenotifyinggateway/coaphttpproxy/MessageHandlerTest.kt
@@ -1,6 +1,7 @@
 package org.gxf.standalonenotifyinggateway.coaphttpproxy
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper
 import org.gxf.standalonenotifyinggateway.coaphttpproxy.coap.MessageHandler
 import org.gxf.standalonenotifyinggateway.coaphttpproxy.coap.exception.InvalidMessageException
 import org.gxf.standalonenotifyinggateway.coaphttpproxy.coap.validation.MessageValidator
@@ -9,10 +10,11 @@ import org.gxf.standalonenotifyinggateway.coaphttpproxy.http.HttpClient
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.*
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.check
+
 
 @ExtendWith(MockitoExtension::class)
 class MessageHandlerTest {
@@ -27,15 +29,14 @@ class MessageHandlerTest {
     private lateinit var messageHandler: MessageHandler
 
     private val testJsonNode = ObjectMapper().readTree("{\"ID\": 12345}")
+    private val testCbor = CBORMapper().writeValueAsBytes(testJsonNode)
 
     @Test
     fun shouldThrowWhenInvalidMessage() {
-        val message = Message("12345", testJsonNode)
-
-        Mockito.`when`(messageValidator.isValid(message)).thenReturn(false)
+        Mockito.`when`(messageValidator.isValid(any<Message>())).thenReturn(false)
 
         Assertions.assertThrows(InvalidMessageException::class.java) {
-            messageHandler.handlePost(message)
+            messageHandler.handlePost("12345", testCbor)
         }
     }
 
@@ -43,10 +44,13 @@ class MessageHandlerTest {
     fun callHttpClientWhenMessageIsValid() {
         val message = Message("12345", testJsonNode)
 
-        Mockito.`when`(messageValidator.isValid(message)).thenReturn(true)
+        Mockito.`when`(messageValidator.isValid(any<Message>())).thenReturn(true)
 
-        messageHandler.handlePost(message)
+        messageHandler.handlePost("12345", testCbor)
 
-        Mockito.verify(httpClient).post(message)
+        Mockito.verify(httpClient).post(check {
+            Assertions.assertEquals(message.deviceId, it.deviceId)
+            Assertions.assertEquals(message.payload, it.payload)
+        })
     }
 }
