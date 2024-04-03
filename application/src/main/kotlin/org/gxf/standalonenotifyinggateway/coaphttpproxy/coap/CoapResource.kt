7f3daa1603181c4a42489107e8b0f5b6cdeb0324
Loes Immens <loes.immens@alliander.com>
2024-04-03 11:01:49 +0200

    FDP-1961: deviceId as parameter
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/main/kotlin/org/gxf/standalonenotifyinggateway/coaphttpproxy/coap/CoapResource.kt
+++ application/src/main/kotlin/org/gxf/standalonenotifyinggateway/coaphttpproxy/coap/CoapResource.kt
@@ -38,7 +38,7 @@ class CoapResource(private val coapProps: CoapProperties, private val messageHan
             }
             val response = messageHandler.handlePost(deviceId, coapExchange.requestPayload)
             // Intentional exception throwing when the response is null or when there is no body
-            writeResponse(coapExchange, response?.body!!)
+            writeResponse(coapExchange, response?.body!!, deviceId)
         } catch (e: Exception) {
             logger.warn { "Error occurred while handling post to device service for device $deviceId" }
             when (e) {
@@ -62,8 +62,8 @@ class CoapResource(private val coapProps: CoapProperties, private val messageHan
         }
     }
 
-    private fun writeResponse(coapExchange: CoapExchange, body: String) {
-        logger.info { "Sending successful response for device ${getIdFromRequestContext(coapExchange)}" }
+    private fun writeResponse(coapExchange: CoapExchange, body: String, deviceId: String) {
+        logger.info { "Sending successful response for device $deviceId" }
         coapExchange.setMaxAge(1)
 
         coapExchange.setETag(
