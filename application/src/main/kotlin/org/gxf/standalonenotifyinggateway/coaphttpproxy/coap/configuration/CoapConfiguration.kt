58e9f2f6dfdf1759247a72b2f9e99606ec0db3b4
Loes Immens <loes.immens@alliander.com>
2024-04-23 11:01:52 +0200

    FDP-1673: spotless apply
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/main/kotlin/org/gxf/standalonenotifyinggateway/coaphttpproxy/coap/configuration/CoapConfiguration.kt
+++ application/src/main/kotlin/org/gxf/standalonenotifyinggateway/coaphttpproxy/coap/configuration/CoapConfiguration.kt
@@ -1,12 +1,14 @@
-// SPDX-FileCoponfigyrightText: Contributors to the GXF project
+// SPDX-FileCopyrightText: Contributors to the GXF project
 //
 // SPDX-License-Identifier: Apache-2.0
-
 package org.gxf.standalonenotifyinggateway.coaphttpproxy.coap.configuration
 
+import java.net.InetSocketAddress
+import java.util.concurrent.TimeUnit
 import org.eclipse.californium.core.config.CoapConfig
 import org.eclipse.californium.core.network.CoapEndpoint
 import org.eclipse.californium.elements.config.CertificateAuthenticationMode
+import org.eclipse.californium.elements.config.Configuration as CaliforniumConfiguration
 import org.eclipse.californium.elements.config.SystemConfig
 import org.eclipse.californium.elements.config.TcpConfig
 import org.eclipse.californium.elements.config.UdpConfig
@@ -20,13 +22,13 @@ import org.eclipse.californium.scandium.dtls.pskstore.AdvancedPskStore
 import org.gxf.standalonenotifyinggateway.coaphttpproxy.coap.configuration.properties.CoapProperties
 import org.gxf.standalonenotifyinggateway.coaphttpproxy.coap.configuration.properties.UdpProperties
 import org.springframework.context.annotation.Bean
-import java.net.InetSocketAddress
-import java.util.concurrent.TimeUnit
-import org.eclipse.californium.elements.config.Configuration as CaliforniumConfiguration
 import org.springframework.context.annotation.Configuration as SpringConfiguration
 
 @SpringConfiguration
-class CoapConfiguration(private val coapProps: CoapProperties, private val udpProps: UdpProperties) {
+class CoapConfiguration(
+    private val coapProps: CoapProperties,
+    private val udpProps: UdpProperties
+) {
 
     init {
         DtlsConfig.register()
@@ -37,60 +39,55 @@ class CoapConfiguration(private val coapProps: CoapProperties, private val udpPr
 
     @Bean
     fun serverConfiguration(): CaliforniumConfiguration =
-            CaliforniumConfiguration.createStandardWithoutFile()
-                    .apply {
-                        updateCoapConfigFromProperties(this)
-                        updateUdpConfigFromProperties(this)
-                        updateDtlsConfig(this)
-                    }
+        CaliforniumConfiguration.createStandardWithoutFile().apply {
+            updateCoapConfigFromProperties(this)
+            updateUdpConfigFromProperties(this)
+            updateDtlsConfig(this)
+        }
 
     fun updateCoapConfigFromProperties(config: CaliforniumConfiguration) {
-        config.set(CoapConfig.MAX_ACTIVE_PEERS, coapProps.maxActivePeers)
-                .set(CoapConfig.MAX_RESOURCE_BODY_SIZE, coapProps.maxResourceBodySize)
-                .set(CoapConfig.MAX_MESSAGE_SIZE, coapProps.maxMessageSize)
-                .set(CoapConfig.PREFERRED_BLOCK_SIZE, coapProps.preferredBlockSize)
-                .set(CoapConfig.DEDUPLICATOR, coapProps.deduplicator)
-                .set(CoapConfig.COAP_SECURE_PORT, coapProps.coapsPort)
-                .set(
-                        CoapConfig.MAX_PEER_INACTIVITY_PERIOD,
-                        coapProps.maxPeerInactivityPeriod.seconds,
-                        TimeUnit.SECONDS
-                )
+        config
+            .set(CoapConfig.MAX_ACTIVE_PEERS, coapProps.maxActivePeers)
+            .set(CoapConfig.MAX_RESOURCE_BODY_SIZE, coapProps.maxResourceBodySize)
+            .set(CoapConfig.MAX_MESSAGE_SIZE, coapProps.maxMessageSize)
+            .set(CoapConfig.PREFERRED_BLOCK_SIZE, coapProps.preferredBlockSize)
+            .set(CoapConfig.DEDUPLICATOR, coapProps.deduplicator)
+            .set(CoapConfig.COAP_SECURE_PORT, coapProps.coapsPort)
+            .set(
+                CoapConfig.MAX_PEER_INACTIVITY_PERIOD,
+                coapProps.maxPeerInactivityPeriod.seconds,
+                TimeUnit.SECONDS)
     }
 
     fun updateUdpConfigFromProperties(config: CaliforniumConfiguration) {
-        config.set(UdpConfig.UDP_RECEIVE_BUFFER_SIZE, udpProps.udpReceiveBufferSize)
-                .set(UdpConfig.UDP_SEND_BUFFER_SIZE, udpProps.udpSendBufferSize)
-                .set(
-                        SystemConfig.HEALTH_STATUS_INTERVAL,
-                        udpProps.healthStatusIntervalInSeconds.seconds,
-                        TimeUnit.SECONDS
-                )
+        config
+            .set(UdpConfig.UDP_RECEIVE_BUFFER_SIZE, udpProps.udpReceiveBufferSize)
+            .set(UdpConfig.UDP_SEND_BUFFER_SIZE, udpProps.udpSendBufferSize)
+            .set(
+                SystemConfig.HEALTH_STATUS_INTERVAL,
+                udpProps.healthStatusIntervalInSeconds.seconds,
+                TimeUnit.SECONDS)
     }
 
     fun updateDtlsConfig(config: CaliforniumConfiguration) {
-        config.set(DtlsConfig.DTLS_ROLE, DtlsRole.SERVER_ONLY)
-                .set(DtlsConfig.DTLS_RECOMMENDED_CIPHER_SUITES_ONLY, false)
-                .set(DtlsConfig.DTLS_PRESELECTED_CIPHER_SUITES, listOf(TLS_PSK_WITH_AES_128_CBC_SHA256))
-                .set(DtlsConfig.DTLS_CIPHER_SUITES, listOf(TLS_PSK_WITH_AES_128_CBC_SHA256))
-                .set(DtlsConfig.DTLS_CLIENT_AUTHENTICATION_MODE, CertificateAuthenticationMode.NONE)
+        config
+            .set(DtlsConfig.DTLS_ROLE, DtlsRole.SERVER_ONLY)
+            .set(DtlsConfig.DTLS_RECOMMENDED_CIPHER_SUITES_ONLY, false)
+            .set(DtlsConfig.DTLS_PRESELECTED_CIPHER_SUITES, listOf(TLS_PSK_WITH_AES_128_CBC_SHA256))
+            .set(DtlsConfig.DTLS_CIPHER_SUITES, listOf(TLS_PSK_WITH_AES_128_CBC_SHA256))
+            .set(DtlsConfig.DTLS_CLIENT_AUTHENTICATION_MODE, CertificateAuthenticationMode.NONE)
     }
 
     @Bean
     fun coapEndpoint(config: CaliforniumConfiguration, dtlsConnector: DTLSConnector): CoapEndpoint =
-            CoapEndpoint.Builder()
-                    .setConfiguration(config)
-                    .setConnector(dtlsConnector)
-                    .build()
+        CoapEndpoint.Builder().setConfiguration(config).setConnector(dtlsConnector).build()
 
     @Bean
     fun dtlsConnector(config: CaliforniumConfiguration, remotePskStore: AdvancedPskStore) =
-            DTLSConnector(
-                    DtlsConnectorConfig
-                            .builder(config)
-                            .setAddress(InetSocketAddress(coapProps.coapsPort))
-                            .setAdvancedPskStore(remotePskStore)
-                            .setConnectionListener(MdcConnectionListener())
-                            .build()
-            )
+        DTLSConnector(
+            DtlsConnectorConfig.builder(config)
+                .setAddress(InetSocketAddress(coapProps.coapsPort))
+                .setAdvancedPskStore(remotePskStore)
+                .setConnectionListener(MdcConnectionListener())
+                .build())
 }
