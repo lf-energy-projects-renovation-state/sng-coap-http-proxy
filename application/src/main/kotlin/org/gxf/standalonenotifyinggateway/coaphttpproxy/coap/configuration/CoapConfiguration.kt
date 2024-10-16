f20f2b85cfbc4e0e8f94bb00b943e59dfa0a7346
Levi Hoogenberg <levi.hoogenberg@alliander.com>
2024-10-16 16:48:04 +0200

    FDP-2407: reuse cipher suite property
    Signed-off-by: Levi Hoogenberg <levi@jpoint.nl>

--- application/src/main/kotlin/org/gxf/standalonenotifyinggateway/coaphttpproxy/coap/configuration/CoapConfiguration.kt
+++ application/src/main/kotlin/org/gxf/standalonenotifyinggateway/coaphttpproxy/coap/configuration/CoapConfiguration.kt
@@ -17,7 +17,6 @@ import org.eclipse.californium.scandium.MdcConnectionListener
 import org.eclipse.californium.scandium.config.DtlsConfig
 import org.eclipse.californium.scandium.config.DtlsConfig.DtlsRole
 import org.eclipse.californium.scandium.config.DtlsConnectorConfig
-import org.eclipse.californium.scandium.dtls.cipher.CipherSuite.TLS_PSK_WITH_AES_128_GCM_SHA256
 import org.eclipse.californium.scandium.dtls.pskstore.AdvancedPskStore
 import org.gxf.standalonenotifyinggateway.coaphttpproxy.coap.configuration.properties.CoapProperties
 import org.gxf.standalonenotifyinggateway.coaphttpproxy.coap.configuration.properties.UdpProperties
@@ -37,36 +36,33 @@ class CoapConfiguration(private val coapProps: CoapProperties, private val udpPr
     @Bean
     fun serverConfiguration(): CaliforniumConfiguration =
         CaliforniumConfiguration.createStandardWithoutFile().apply {
-            updateCoapConfigFromProperties(this)
-            updateUdpConfigFromProperties(this)
-            updateDtlsConfig(this)
+            updateCoapConfigFromProperties()
+            updateUdpConfigFromProperties()
+            updateDtlsConfig()
         }
 
-    fun updateCoapConfigFromProperties(config: CaliforniumConfiguration) {
-        config
-            .set(CoapConfig.MAX_ACTIVE_PEERS, coapProps.maxActivePeers)
-            .set(CoapConfig.MAX_RESOURCE_BODY_SIZE, coapProps.maxResourceBodySize)
-            .set(CoapConfig.MAX_MESSAGE_SIZE, coapProps.maxMessageSize)
-            .set(CoapConfig.PREFERRED_BLOCK_SIZE, coapProps.preferredBlockSize)
-            .set(CoapConfig.DEDUPLICATOR, coapProps.deduplicator)
-            .set(CoapConfig.COAP_SECURE_PORT, coapProps.coapsPort)
-            .set(CoapConfig.MAX_PEER_INACTIVITY_PERIOD, coapProps.maxPeerInactivityPeriod.seconds, TimeUnit.SECONDS)
+    private fun CaliforniumConfiguration.updateCoapConfigFromProperties() {
+        set(CoapConfig.MAX_ACTIVE_PEERS, coapProps.maxActivePeers)
+        set(CoapConfig.MAX_RESOURCE_BODY_SIZE, coapProps.maxResourceBodySize)
+        set(CoapConfig.MAX_MESSAGE_SIZE, coapProps.maxMessageSize)
+        set(CoapConfig.PREFERRED_BLOCK_SIZE, coapProps.preferredBlockSize)
+        set(CoapConfig.DEDUPLICATOR, coapProps.deduplicator)
+        set(CoapConfig.COAP_SECURE_PORT, coapProps.coapsPort)
+        set(CoapConfig.MAX_PEER_INACTIVITY_PERIOD, coapProps.maxPeerInactivityPeriod.seconds, TimeUnit.SECONDS)
     }
 
-    fun updateUdpConfigFromProperties(config: CaliforniumConfiguration) {
-        config
-            .set(UdpConfig.UDP_RECEIVE_BUFFER_SIZE, udpProps.udpReceiveBufferSize)
-            .set(UdpConfig.UDP_SEND_BUFFER_SIZE, udpProps.udpSendBufferSize)
-            .set(SystemConfig.HEALTH_STATUS_INTERVAL, udpProps.healthStatusInterval.seconds, TimeUnit.SECONDS)
+    private fun CaliforniumConfiguration.updateUdpConfigFromProperties() {
+        set(UdpConfig.UDP_RECEIVE_BUFFER_SIZE, udpProps.udpReceiveBufferSize)
+        set(UdpConfig.UDP_SEND_BUFFER_SIZE, udpProps.udpSendBufferSize)
+        set(SystemConfig.HEALTH_STATUS_INTERVAL, udpProps.healthStatusInterval.seconds, TimeUnit.SECONDS)
     }
 
-    fun updateDtlsConfig(config: CaliforniumConfiguration) {
-        config
-            .set(DtlsConfig.DTLS_ROLE, DtlsRole.SERVER_ONLY)
-            .set(DtlsConfig.DTLS_RECOMMENDED_CIPHER_SUITES_ONLY, false)
-            .set(DtlsConfig.DTLS_PRESELECTED_CIPHER_SUITES, listOf(TLS_PSK_WITH_AES_128_GCM_SHA256))
-            .set(DtlsConfig.DTLS_CIPHER_SUITES, coapProps.cipherSuites)
-            .set(DtlsConfig.DTLS_CLIENT_AUTHENTICATION_MODE, CertificateAuthenticationMode.NONE)
+    private fun CaliforniumConfiguration.updateDtlsConfig() {
+        set(DtlsConfig.DTLS_ROLE, DtlsRole.SERVER_ONLY)
+        set(DtlsConfig.DTLS_RECOMMENDED_CIPHER_SUITES_ONLY, false)
+        set(DtlsConfig.DTLS_PRESELECTED_CIPHER_SUITES, coapProps.cipherSuites)
+        set(DtlsConfig.DTLS_CIPHER_SUITES, coapProps.cipherSuites)
+        set(DtlsConfig.DTLS_CLIENT_AUTHENTICATION_MODE, CertificateAuthenticationMode.NONE)
     }
 
     @Bean
